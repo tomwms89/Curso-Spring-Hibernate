f58cb6e8087a4ee0584a65e3b196d7d5fd52c6b0
Wellington Macedo da Silva <wellington.macedo89@outlook.com>
2020-05-15 13:49:15 -0300

    update

---
+++ web-customer-tracker/src/com/qintess/testdb/TestDbServlet.java
@@ -0,0 +1,55 @@
+package com.qintess.testdb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
+
+/**
+ * Servlet implementation class TestDbServlet
+ */
+@WebServlet("/TestDbServlet")
+public class TestDbServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String user = "postgres";
+		String pass = "1234";
+		
+		String jdbcUrl = "jdbc:postgresql://localhost:5432/web_customer_tracker";
+		String driver = "org.postgresql.Driver";
+		
+		
+		// conectando ao banco de dados
+		try {
+			PrintWriter out = response.getWriter();
+			
+			out.println("Connecting to database: " + jdbcUrl);
+			
+			Class.forName(driver);
+			
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			out.println("SUCESS!!!");
+			
+			myConn.close();
+			
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		
+		
+		
+	}
+
+}
